5cb35a65ee5f353543d9f3b63de048b1c3c436a5
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-08-24 22:46:35 +0530

    Add React Router for navigation and update component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Recommended from "./Components/Recommended";
+import RestaurantList from "./Components/RestaurantList";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <div className="divider mb-8 mt-14 mx-4">RECOMMENDED</div>
+              <Recommended />
+              <div className="divider mb-8 mt-14 mx-4">Explore Restaurants</div>
+              <RestaurantList />
+            </>
+          }
+        />
+        <Route path="/restaurant/:id" element={<Menu />} />
+      </Routes>
+    </Router>
   );
 }
 
